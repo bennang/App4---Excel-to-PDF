4869b6618348b6b134b35ab9c40941a40bbdf3e8
ben <benjaminazj@gmail.com>
2022-11-24 16:03:31 +0800

    Day 25: Convert Excel files into PDFs

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
+    filename = Path(filepath).stem
+    invoice_no = filename.split("-")[0]
+    pdf = FPDF(orientation='P', unit='mm', format='A4')
+    pdf.add_page()
+    pdf.set_font(family='Times', style='B', size=16)
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_no}")
+    pdf.output(f"PDFs/{filename}.pdf")
